1176caf23092ce3d8eab4b1717de45e46259446d
mr-wang1009 <1115378579@qq.com>
2022-07-01 14:45:51 +0800

    fix: css import url

--- components/main-head.tsx
+++ components/main-head.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import styles from "../../styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 import { useState } from "react";
 
 interface Props {
